c1e6e94f492534b59a1f409b75c9b8ace215000e
Alexander Rowe <alexrowe707@gmail.com>
2024-10-07 18:49:09 -0700

    Warmstart MVP
    Replaces creating an EC2 instance with starting one from an autoscaling
    warm group

--- src/index.js
+++ src/index.js
@@ -2,27 +2,39 @@ const aws = require('./aws');
 const gh = require('./gh');
 const config = require('./config');
 const core = require('@actions/core');
-
-function setOutput(label, ec2InstanceId) {
-  core.setOutput('label', label);
-  core.setOutput('ec2-instance-id', ec2InstanceId);
-}
+const github = require('@actions/github');
 
 async function start() {
-  const label = config.generateUniqueLabel();
-  const githubRegistrationToken = await gh.getRegistrationToken();
-  const ec2InstanceId = await aws.startEc2Instance(label, githubRegistrationToken);
-  setOutput(label, ec2InstanceId);
+  const runId = github.context.runId;
+  const groupName = config.input.ec2AutoScalingGroupName;
+  let ec2InstanceId = await aws.startStoppedInstanceInAutoScalingGroup(groupName, runId);
+
+  // If did not start, start one from cold, get ID
+  if (!ec2InstanceId) {
+    core.info("Could not launch from AutoScaling Group, attempting cold start");
+    const template = await aws.getLaunchTemplateFromASG(config.input.ec2AutoScalingGroupName);
+    ec2InstanceId = await aws.startEc2Instance(template.id, template.version, runId);
+  }
+
+  if (!ec2InstanceId) {
+    throw Error("Could not start instance");
+  }
+
+  core.setOutput('ec2-instance-id', ec2InstanceId);
+
   await aws.waitForInstanceRunning(ec2InstanceId);
-  await gh.waitForRunnerRegistered(label);
+  await aws.startRunnerCommand(ec2InstanceId);
+  await gh.waitForRunnerRegistered(ec2InstanceId);
 }
 
 async function stop() {
-  await aws.terminateEc2Instance();
-  await gh.removeRunner();
+  const ec2InstanceId = config.input.ec2InstanceId;
+  core.info(`Stopping ${ec2InstanceId}`);
+  await aws.terminateEc2Instance(ec2InstanceId);
+  await gh.removeRunner(ec2InstanceId);
 }
 
-(async function () {
+(async function() {
   try {
     config.input.mode === 'start' ? await start() : await stop();
   } catch (error) {
